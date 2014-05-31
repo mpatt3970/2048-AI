f8db846ebe4194e1e8287312daece65e44e33e65
Mike Patterson <mipatter@mymail.mines.edu>
2014-05-30 22:32:44 -0600

    Here's aforementioned file.

---
+++ src/mipatter/mines/edu/Optimizer.java
@@ -0,0 +1,15 @@
+package mipatter.mines.edu;
+
+public class Optimizer {
+	/*
+	 * Loop over the game, adjusting values one at a time and keeping track of how the game performs
+	 */
+	
+	private int[] values;
+	private int smooth_weight;
+	private int edge_weight;
+	private int corner_weight;
+	private int score_weight;
+	private int open_tile_weight;
+	
+}
